12270a31fa652622fa1b3270b0eab782a2e6d318
toran billups <toranb@gmail.com>
2012-10-20 20:58:03 -0500

    pulled out the person.js implementation code to get the kata fail ready

--- ember/static/script/app/person.js
+++ ember/static/script/app/person.js
@@ -1,63 +1,39 @@
 Person = (function($) {
 
   var PersonObject = function(params) {
-    this.tbody = params.tbody;
-    this.username = params.username;
-    this.errors = params.errors;
-    this.url = params.url;
+    //write some code to fix the first failing test
   };
 
   PersonObject.prototype.addPerson = function() {
-     var username = this.username.val();
-     if (username.trim() === '') {
-       this.errors.text('please enter a valid username');
-     }else{
-       this.errors.text('');
-       this.doHttpPostWithPersonData(username);
-     }
+    //write some code to fix the next failing test
   };
 
   PersonObject.prototype.doHttpPostWithPersonData = function(username) {
-    var data = {'username': username};
-    var callback = this.addPersonCallback.bind(this);
-    $.post(this.url, data, callback);
+    //write some code to fix the next failing test
   };
 
   PersonObject.prototype.addPersonCallback = function(response) {
-    var person = {'id': response['id'], 'username': response['username']};
-    this.addPersonToHtml(response);
-    this.username.val('');
+    //write some code to fix the next failing test
   };
 
   PersonObject.prototype.addPersonToHtml = function(person) {
-    var row = '<tr id="person_' + person['id'] + '">';
-    row += ('<td>' + person['id'] + '</td>');
-    row += ('<td>' + person['username'] + '</td>');
-    row += ('<td><a href="#" onclick="person.removePerson(' + person['id'] + '); return false;">delete</a></td>');
-    row += ('</tr>');
-    this.tbody.append(row);
+    //write some code to fix the next failing test
   };
 
   PersonObject.prototype.findAll = function() {
-    var callback = this.findAllCallback.bind(this);
-    $.getJSON(this.url, callback);
+    //write some code to fix the next failing test
   };
 
   PersonObject.prototype.findAllCallback = function(response) {
-    var self = this;
-    response.forEach(function(person) {
-      self.addPersonToHtml(person);
-    });
+    //write some code to fix the next failing test
   };
 
   PersonObject.prototype.removePerson = function(personId) {
-    var url = this.url + '/' + personId;
-    var callback = this.removePersonCallback.bind(this, personId);
-    $.ajax({type: 'DELETE', url: url, success: callback});
+    //write some code to fix the next failing test
   };
 
   PersonObject.prototype.removePersonCallback = function(personId) {
-    $('table#people').find('tbody>tr[id="person_' + personId + '"]').remove();
+    //write some code to fix the next failing test
   };
 
   return PersonObject;
